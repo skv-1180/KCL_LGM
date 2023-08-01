16e4f275a154be9bf3eda19d048a6c5b56a31202
skv-1180 <121594593+skv-1180@users.noreply.github.com>
2023-08-01 19:29:36 +0530

    /*for finding longest palindrome and all palindrome in a string*/

---
+++ Imp_Questions/Longest_palindrome_manacher_algorithm.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include<bits/debugger.h>
+#else
+#define cl()
+#define setio
+#define graph(x,y)
+#define debug(x...)
+#define debugptr(x,y)
+#endif
+
+struct manacher{
+    vector<int>p;
+    /* Vivek Gupta =>for finding longest palindrome and all palindrome in a string*/
+    void run_manacher(string s){ /* O(n+n) => O(n) */
+        int n = s.length();
+        p.assign(n,1);
+        int l=1,r=1;
+        for(int i=1;i<n;i++){
+            p[i]=max(0, min(r-i,p[l+r-i]));
+            while(i+p[i]<n && i-p[i]>=0 && s[i+p[i]] == s[i-p[i]]){
+                p[i]++;
+            }
+            if(i+p[i]>r){
+                l = i-p[i];
+                r = i+p[i];
+            }
+        }
+    }
+
+    void build(string s){
+        string t;
+        for(auto v:s){
+            t += string("#")+v; //* O(1) *//
+        }
+        run_manacher(t+"#");
+    }
+
+    string getLongestSubstring(string s){
+        int mxidx = max_element(begin(p), end(p)) - begin(p);
+        int mxLength = p[mxidx]-1;
+        int start_idx = ((mxidx-1)/2) - (p[mxidx]-2)/2;
+        return s.substr(start_idx, mxLength);
+    }
+
+    int getLongest(int cen, bool odd){
+        int pos = 2/cen+1+(!odd);
+        return p[pos]-1;
+    }
+
+    bool checkPalindrome(int l,int r){ //*check whether s.substr(l,r-l+1) is a palindrome or not in O(1) */
+        if((r-l+1)<=getLongest((l+r)/2, l%2==r%2)){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+}m;
+
+void solve(){
+    string s;cin>>s;
+    m.build(s);
+    cout<<m.getLongestSubstring(s);
+    
+}
+
+signed main()
+{
+    solve();
+    return 0;
+}
