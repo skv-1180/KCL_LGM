7139b27ef796013c839deb1bc36727b8b1a84e24
CodingLord108 <125797762+CodingLord108@users.noreply.github.com>
2023-09-03 18:34:59 +0530

    Update C_Fermat_s_Last_Theorem.cpp

--- C_Fermat_s_Last_Theorem.cpp
+++ C_Fermat_s_Last_Theorem.cpp
@@ -1,62 +1,52 @@
-/* author:  Shubham Kr. Verma */
-#include<bits/stdc++.h>
-using namespace std;
-const int N=1e9+7;
-#define endl '\n'
-#define int long long
-#define vi vector<int>
-#define pii pair<int,int>
-#define all(v)begin(v),end(v)
-#define cint(x)int x; cin>>x;
-#define cstr(x)string x;cin>>x;
-#define ccint(x,y)int x,y;cin>>x>>y;
-#define ccstr(x,y)string x,y;cin>>x>>y;
-#define print(v)for(auto x:v)cout<<x<<' ';cout<<endl;
-#define cvect(v,n)vector<int>v(n);for(auto&x:v)cin>>x;
-
-#ifndef ONLINE_JUDGE
-#include<bits/debugger.h>
-#else
-#define graph(x,y)
-#define debug(x...)
-#endif
-
-
-int tl;
-
-bool chk1(int m){
-    int p=(m*(m+1))/2;
-    return (p*p-9)<tl;
-}
-
-int bs1(){
-    int s = 0,e=1e4+10;
-    int ans=0;
-    while(s<=e){
-        int m=(s+e)/2;
-        // debug(m,tl)
-        if(chk1(m)){
-            ans=m;
-            s=m+1;
-        }else{
-            e=m-1;
+//Preprocessing:T.C.:O(N)      M.C.:O(4*N)
+//excessing    :T.C.:O(log(N)) M.C.:O(1)
+//note your vector should be 1 indexed and query 1 indexed;
+class st{
+    private:
+        pii id = {-1,-1};//identity of func f
+        vector<pii>t;int n=0;int si=0;
+        pii f(pii a,pii b){
+            return max(a,b);
         }
-    }
-    int p=(ans*(ans+1))/2;
-    tl-=((p*p-9));
-    ans++;
-    return ans;
-}
-
-
-signed main(){
-    ios_base::sync_with_stdio(false);cin.tie(NULL);
-    
-    ccint(l,r)
-    tl=l;
-    int a=1,b=1,c=1,n=3;
-    n=bs1();
-    debug(n,tl)
-    
-return 0;
-}
+        void build(vector<pii>&a, int v, int tl, int tr) {
+            if (tl == tr)t[v] = a[tl];
+            else {
+                int tm = (tl + tr) / 2;
+                build(a, v*2, tl, tm);
+                build(a, v*2+1, tm+1, tr);
+                t[v] = f(t[v*2] , t[v*2+1]);
+            }
+        }
+        
+        pii func(int v, int tl, int tr, int l, int r){
+            if (l > r) return id;
+            if (l == tl && r == tr)return t[v];
+            int tm = (tl + tr)>>1;
+            return f(func(v*2, tl, tm, l, min(r, tm)),func(v*2+1, tm+1, tr, max(l, tm+1), r));
+        }
+        void modify(int v, int tl, int tr, int pos, pii new_val) {
+            if (tl == tr) t[v] = new_val;
+            else {
+                int tm = (tl + tr) / 2;
+                if (pos <= tm)modify(v*2, tl, tm, pos, new_val);
+                else     modify(v*2+1,tm+1,tr,pos, new_val);
+                t[v] = f(t[v*2] , t[v*2+1]);
+            }
+        }
+    public:
+        st(vector<pii>&a,int MAXN){
+            t.resize(4*MAXN);si=1;n=a.size();//n=n+1
+            build(a,1ll,si,n-1);
+            // debug(t)
+        }
+        void update(int position,pii new_value){
+            modify(1ll,si,n-1,position,new_value);
+        }
+        pii query(int l,int r){
+            return func(1ll,si,n-1,l,r);
+        }
+};
+//st s(v,10);//v is your vector 10 is its max size
+//s.query(2,5);//it will give f aplied at 2 ,3 , 4,5
+//s.update(a,b);//it will update ath number to b in our data
+//for convertig to 0 indexing make si=0 && take input as 0 indexing and pass --l,--r,--u//u->update index
